10960af8963d5c8e520e9095a660636f497b91ab
Carl F. Corneil <aecorn@gmail.com>
2022-10-28 21:16:35 +0200

    apidata functions added to star import

--- statbank/__init__.py
+++ statbank/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["StatbankClient"]
+__all__ = ["StatbankClient", "apidata", "apidata_all", "apidata_rotate"]
 
 from .client import StatbankClient
+from .apidata import apidata, apidata_all, apidata_rotate

ffeadde447733fa82e41a8e213d64beda0715224
Damir Medakovic <dam.medakovic@gmail.com>
2022-10-28 12:06:14 +0200

    Fix relative import issue

--- tests/test_statbank.py
+++ tests/test_statbank.py
@@ -7,7 +7,7 @@ import pandas as pd
 import pytest
 import requests
 
-from ..statbank.client import StatbankTransfer, StatbankUttrekksBeskrivelse
+from statbank.client import StatbankTransfer, StatbankUttrekksBeskrivelse
 
 
 def fake_mail():

73949b42a8c2bed5d602a8079c607d9146cb2ab4
Carl F. Corneil <aecorn@gmail.com>
2023-12-15 08:47:40 +0100

    init fix pre-commit, add logger

--- src/statbank/__init__.py
+++ src/statbank/__init__.py
@@ -1,6 +1,44 @@
-__version__ = "1.0.9"
+"""Validates and transfers data from Dapla to Statbank. Gets data from public and internal statbank.
+
+Used internally by SSB (Statistics Norway).
+"""
+from __future__ import annotations
+
+import importlib
+import logging
+
+import toml
+
+from statbank.apidata import apidata
+from statbank.apidata import apidata_all
+from statbank.apidata import apidata_rotate
+from statbank.client import StatbankClient
 
 __all__ = ["StatbankClient", "apidata", "apidata_all", "apidata_rotate"]
 
-from statbank.apidata import apidata, apidata_all, apidata_rotate
-from statbank.client import StatbankClient
+logger = logging.getLogger(__name__)
+
+
+# Split into function for testing
+def _try_getting_pyproject_toml(e: Exception | None = None) -> str:
+    if e is None:
+        passed_excep: Exception = Exception("")
+    else:
+        passed_excep = e
+    try:
+        version: str = toml.load("../pyproject.toml")["tool"]["poetry"]["version"]
+    except toml.TomlDecodeError as e:
+        version = "0.0.0"
+        logger.exception(
+            "Error from dapla-statbank-clients __init__, not able to get version-number, setting it to %s. Exception: %s",
+            version,
+            str(passed_excep),
+        )
+    return version
+
+
+# Gets the installed version from pyproject.toml, then there is no need to update this file
+try:
+    __version__ = importlib.metadata.version("dapla-statbank-client")
+except importlib.metadata.PackageNotFoundError as e:
+    __version__ = _try_getting_pyproject_toml(e)

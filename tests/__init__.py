0979c3f4d78865357942c2f5b7e8eba96261dd0c
Carl F. Corneil <aecorn@gmail.com>
2024-02-13 14:25:24 +0100

    ruff apidata tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,11 @@
 """The tests for statbank client should be run by pytest through nox."""
+
+import os
+
+os.environ["STATBANK_BASE_URL"] = "https://test_fake_url/"
+os.environ["STATBANK_ENCRYPT_URL"] = "https://fake_url2/"
+os.environ["JUPYTERHUB_USER"] = "ssb@ssb.no"
+
+os.environ["DAPLA_ENVIRONMENT"] = "TEST"
+os.environ["DAPLA_SERVICE"] = "FAKE"
+os.environ["DAPLA_REGION"] = "PYTEST"

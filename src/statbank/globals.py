28ed31164ca1e53dc9b3e16d281d3fd6548c77f0
Carl F. Corneil <aecorn@gmail.com>
2024-02-14 09:22:44 +0100

    wrong timezone calculation

--- src/statbank/globals.py
+++ src/statbank/globals.py
@@ -1,6 +1,6 @@
 import datetime as dt
 
-OSLO_TIMEZONE = dt.timezone(dt.timedelta(hours=1))
+OSLO_TIMEZONE = dt.timezone.utc + dt.timedelta(hours=1)
 TOMORROW = dt.datetime.now(tz=OSLO_TIMEZONE) + dt.timedelta(days=1)
 APPROVE_DEFAULT_JIT = 2
 STATBANK_TABLE_ID_LEN = 5

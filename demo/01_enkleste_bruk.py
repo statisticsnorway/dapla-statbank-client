42698ed67d4aa072d925d41357f46349107ea23b
Carl Corneil <cfc@ssb.no>
2024-08-28 10:43:18 +0200

    convert demo notebooks to percent format

---
+++ demo/01_enkleste_bruk.py
@@ -0,0 +1,62 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: dapla-statbank-client
+#     language: python
+#     name: dapla-statbank-client
+# ---
+
+# %% [markdown]
+# # Enkleste bruk av pakken med "ferdig data"
+#
+# Pass på at du har innstallert pakken i en "kernel" du kan bruke i notebooket
+
+# %%
+import pandas as pd
+
+from statbank import StatbankClient
+
+# %% [markdown]
+# ### Få tak i data
+# Hovedtabell 07495 har to deltabeller, du har nok ikke tilgang på disse filene. \
+# Du bør finne egen data, kanskje fra fjoråret, og bruke seksjonens lastebruker osv. \
+# Endre koden til noe du kan teste med.
+
+# %%
+df_07495_fylker = pd.read_parquet("07495_statbank_fylker.parquet")
+df_07495_landet = pd.read_parquet("07495_statbank_landet.parquet")
+
+# %% [markdown]
+# ### Lag statbank-client
+# Din lastebruker vil være anderledes
+
+# %%
+client = StatbankClient()
+
+# %% [markdown]
+# ### Vi må vite hvilke deltabeller hver dataframe representerer
+# Din hovedtabell har kanskje bare èn deltabell, men vi trenger fortsatt navnet på den.
+
+# %%
+data_07495 = {"kargrs01fylker1.dat": df_07495_fylker,
+              "kargrs01landet1.dat": df_07495_landet }
+
+# %% [markdown]
+# ### Du kan validere rett på clienten?
+
+# %%
+client.validate(data_07495, "07495")
+
+# %% [markdown]
+# ### Overfør data med .transfer()
+# Om du har "hovedtabellnavn" istedenfor "tabell-id" så skal det i teorien fungere (kanskje også for interntabeller)
+
+# %%
+client.transfer(data_07495, "07495")
---
+++ demo/03_alleparametre_datewidget.py
@@ -0,0 +1,82 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: dapla-statbank-client
+#     language: python
+#     name: dapla-statbank-client
+# ---
+
+# %%
+import pandas as pd
+from statbank import StatbankClient
+
+# %% [markdown]
+# ### Create client
+
+# %%
+client = StatbankClient(#date="2023-02-21",
+                        shortuser="cfc",
+                        cc="thu",
+                        bcc="tir",
+                        overwrite=True,
+                        approve=2)
+
+# %% [markdown]
+# ### Sett dato interaktivt med widget
+
+# %%
+date = client.date_picker()
+
+# %%
+client.set_publish_date(date)
+
+# %%
+print(client)
+
+# %%
+client.log
+
+# %% [markdown]
+# ### Get description
+
+# %%
+filbeskrivelse_07495 = client.get_description(tableid="07495")
+
+# %% [markdown]
+# ### Load data
+
+# %%
+df_07495_fylker = pd.read_parquet("07495_statbank_fylker.parquet")
+df_07495_landet = pd.read_parquet("07495_statbank_landet.parquet")
+
+# %%
+data_07495 = {"kargrs01fylker1.dat" : df_07495_fylker,
+              "kargrs01landet1.dat" : df_07495_landet}
+
+# %% [markdown]
+# ### Validate data
+
+# %%
+filbeskrivelse_07495.validate(data_07495, raise_errors=True)
+
+# %% [markdown]
+# ### Round data
+
+# %%
+data_07495["kargrs01fylker1.dat"]
+
+# %%
+data_07495 = filbeskrivelse_07495.round_data(data_07495)
+
+# %% [markdown]
+# ### Transfer (actually sending the data)
+
+# %%
+transfer_07495 = client.transfer(data_07495, tableid="07495")
